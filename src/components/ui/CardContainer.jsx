ada79bfffab950a0a884854b050faca211fd6e8d
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-05-16 13:45:32 -0400

    feat: add loader component

--- src/components/ui/CardContainer.jsx
+++ src/components/ui/CardContainer.jsx
@@ -1,20 +1,24 @@
 // CardContainer.js
 import Card from './Card';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigation } from 'react-router-dom';
 import { getTours } from '../../utils/api';
+import Loader from './Loader';
 
 function CardContainer() {
   const data = useLoaderData();
+  const navigate = useNavigation();
 
   return (
     <div className="overview">
-      (
-      <div className="card-container">
-        {data.tours.map((tour) => (
-          <Card key={tour.id} {...tour} />
-        ))}
-      </div>
-      )
+      {navigate.state === 'loading' ? (
+        <Loader />
+      ) : (
+        <div className="card-container">
+          {data.tours.map((tour) => (
+            <Card key={tour.id} {...tour} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
